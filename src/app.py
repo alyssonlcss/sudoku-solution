2f86593fd6607a10ba4ea9c8f3e9972dfcfc2a9c
alyssonlcss <alyssonlcss@gmail.com>
2022-01-31 15:39:56 -0300

    add func - loadingMatrix(), viewMatrix

--- src/app.py
+++ src/app.py
@@ -9,8 +9,24 @@ def readFile(pathFile):
     file.close()
     return fileList
 
+def viewMatrix(matrix):
+    for l in range(9):
+        for c in range(9):
+            print(f'[{matrix[l][c]}]', end=' ')
+        print()
+    matrix.clear()
+    print()
 
-def addMatrix(fileList):
+
+def lesserSpace(matrix):
+    countVoidC, countVoidL = 0, 0
+    for c in range(9):
+        for l in range(9):
+            if matrix[c][l] == 0:
+                countVoidL += 1
+
+
+def loadingMatrix(fileList):
     size = len(fileList)
     sudokuMatrix = []
     
@@ -18,13 +34,8 @@ def addMatrix(fileList):
         if not i % 10 == 9:
             sudokuMatrix.append(fileList[i].split(" "))
         if i % 10 == 9: # We will call the function solve in this if
-            for l in range(9):
-                for c in range(9):
-                    print(f'[{sudokuMatrix[l][c]}]', end=' ')
-                print()
-            sudokuMatrix.clear()
-            print()
+            viewMatrix(sudokuMatrix)
 
 
 fileList = readFile("files/tests.txt")
-addMatrix(fileList)
+loadingMatrix(fileList)
