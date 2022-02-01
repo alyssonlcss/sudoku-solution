a3be8372ca7168a76eef263b2921963b250660af
alyssonlcss <alyssonlcss@gmail.com>
2022-02-01 15:30:25 -0300

    ff

--- src/temp.py
+++ src/temp.py
@@ -8,8 +8,17 @@ list = [[0,0,3,0,0,8,0,2,7],
         [5,0,8,2,0,9,7,6,0],
         [0,6,0,0,3,4,5,9,0]]
 
-boo = True
 
-
-if boo == False:
-    print("False")
+def loadingMatrix(fileList):
+    size = len(fileList)
+    matrix_game, list = [], []
+    for i in range(size):
+        if not i % 10 == 9:
+            temp = fileList[i].split(" ")
+            for j in range(9):
+                list.append(int(temp[j]))
+            matrix_game.append(list)
+            list = []
+        if i % 10 == 9: # We will call the function solve in this if
+            solveSudoku(matrix_game)
+            matrix_game.clear()
